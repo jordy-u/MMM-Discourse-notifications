ccebe534703188415414dacc91c8767749dbd19e
OsinixXx <osinix4@gmail.com>
2020-06-15 16:38:09 +0100

    Implement ApiKeyChecker The code verifies if the api key is undefined, empty or in the wrong format. It formats the key to lower case, because that is needed for the Discourse API. Some of the tests have been changed.

--- js/ApiKeyChecker.js
+++ js/ApiKeyChecker.js
@@ -1,58 +1,60 @@
 const apiKeyError = {
 	"NO_ERROR":0,
-	"KEY_IS_UNDEFINED":1,
+	"KEY_IS_UNDEFINED_OR_NOT_A_STRING":1,
 	"KEY_IS_EMPTY":2,
-	"KEY_IS_NOT_ONLY_LOWERCASE_HEXADECIMALS":3,
-	"KEY_DOES_NOT_CONTAIN_EXACTLY_32_CHARACTERS":4
+	"KEY_DOES_NOT_EQUAL_32_HEXADECIMAL_NUMBERS":3
 };
 Object.freeze(apiKeyError);
 
-class ApiKeyChecker {
 
-	//TODO: Implement this function correctly.
-	static checkDiscourseApiKey(apiKey) {
+/**
+ * Check if the apiKey has the right format before using it.
+	 * @param {string} apiKey
+	 * @return {Object} success, error, errorMsg, formattedKey
+	 */
+function checkDiscourseApiKey(apiKey) {
 
-		//check if key is defined
-		if (true) {
-			return {
-				success : false,
-				error : apiKeyError.KEY_IS_UNDEFINED,
-				errorMsg : "'UserApiKey' is not defined in config.js.",
-			};
-		}
+	//check if key is defined
+	if (typeof apiKey == "undefined") {
 
-		//check if key is not empty
-		if (true) {
-			return {
-				success : false,
-				error : apiKeyError.KEY_IS_EMPTY,
-				errorMsg : "'UserApiKey' in config.js is an empty string.",
-			};
-		}
-
-		//check if key is a lowercase hexadecimal string
-		if (true) {
-			return {
-				success : false,
-				error : apiKeyError.KEY_IS_NOT_ONLY_LOWERCASE_HEXADECIMALS,
-				errorMsg : "The provided API key has a wrong format. It should only contain lowercase hexadecimals.",
-			};
-		}
-
-		//check if key has a length of 32
-		if (true) {
-			return {
-				success : false,
-				error : apiKeyError.KEY_DOES_NOT_CONTAIN_EXACTLY_32_CHARACTERS,
-				errorMsg : "Invalid API key length. It should be 32 characters long.",
-			};
-		}
-
-		//Check passed.
 		return {
-			success : true,
-			error : apiKeyError.NO_ERROR,
-			errorMsg : "No error",
+			success : false,
+			error : apiKeyError.KEY_IS_UNDEFINED_OR_NOT_A_STRING,
+			errorMsg : "'UserApiKey' is not defined in config.js or is not a string.",
 		};
 	}
+
+	apiKey = apiKey.toLowerCase();
+
+	//Remove spaces at the start and end of the key.
+	apiKey = apiKey.trim();
+
+	//check if key is not empty
+	if (apiKey.length === 0) {
+		return {
+			success : false,
+			error : apiKeyError.KEY_IS_EMPTY,
+			errorMsg : "'UserApiKey' in config.js is an empty string.",
+		};
+	}
+
+
+	//check if key is a lowercase hexadecimal string
+	if (apiKey.match("^[a-f0-9]{32}$") === null) {
+		return {
+			success : false,
+			error : apiKeyError.KEY_DOES_NOT_EQUAL_32_HEXADECIMAL_NUMBERS,
+			errorMsg : "The provided API key has a wrong format. It should be a string of 32 hexadecimal numbers.",
+		};
+	}
+
+	//Check passed.
+	return {
+		success : true,
+		error : apiKeyError.NO_ERROR,
+		errorMsg : "No error",
+		formattedKey : apiKey,
+	};
 }
+
+module.exports = {apiKeyError, checkDiscourseApiKey};
