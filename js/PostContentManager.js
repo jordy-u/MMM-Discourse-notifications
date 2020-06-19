a11ddc14dbf5d236d7cd0521b50ed9021b158d3d
Jordy Ubink <jordyubink@hotmail.nl>
2020-06-19 17:49:39 +0200

    Make the content manager able to load data.

--- js/PostContentManager.js
+++ js/PostContentManager.js
@@ -1,4 +1,5 @@
 const _ = require("underscore");
+const sleep = require('util').promisify(setTimeout);
 
 /*
 Deze class:
@@ -19,7 +20,15 @@ class PostContentManager {
 
 	//Example of loadedPosts
 
-	constructor() {
+	requestHandler;
+	postContent;
+	currentlyDownloading = false;
+
+	/**
+	 * @param {DiscourseRequestHandler} discourseRequestHandler Connection instance for the Discourse site.
+	 */
+	constructor(discourseRequestHandler) {
+		this.requestHandler = discourseRequestHandler;
 		this.postContent = {};
 		this.order = [0,1,2,3,4];
 	}
@@ -27,6 +36,35 @@ class PostContentManager {
 	updatePostList(listOfPosts) {
 		this.removeReadPosts(listOfPosts);
 		this.addUnreadPosts(listOfPosts);
+	}
+
+	/**
+	 * Load content of the threads in the array order in the background.
+	 * By default, only 20 requests per minute can be made. So There is a delay of 3 seconds between requests.
+	 * @param {int[]} orderedListOfThreads ThreadIds in the orders they need to be loaded.
+	 */
+	async loadContent(orderedListOfThreads) {
+
+		for (const thread of orderedListOfThreads) {
+			let threadContent;
+			try {
+				const postToBeLoaded = Object.keys(this.postContent[thread].posts);
+				threadContent = await this.requestHandler.getPosts(thread, postToBeLoaded);
+			} catch (error) {
+				new Error(error);
+			}
+
+			for (let post of threadContent.post_stream.posts) {
+				this.postContent[thread].posts[post.id] = post;
+			}
+
+			await sleep( 3000);
+		}
+
+	}
+
+	stopDownloading() {
+
 	}
 
 	removeReadPosts(listOfUnreadPosts) {
@@ -57,7 +95,7 @@ class PostContentManager {
 
 			for (let post of posts) {
 				if (this.postContent[thread].posts[post] === undefined) {
-					this.postContent[thread].posts[post] = {data: undefined};
+					this.postContent[thread].posts[post] = {};
 				}
 
 			}
